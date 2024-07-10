7f56fad414e959a97d2c758506e5a0a7b2be43e1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-07-10 11:29:42 -0400

    print more info

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ async function run() {
   try {
     const userSuppliedPrId = core.getInput('pr-id')
     const token = core.getInput('repo-token')
+    console.log('PR ID: %s', userSuppliedPrId)
 
     const prNumber = getPrNumber(userSuppliedPrId)
 
@@ -12,12 +13,16 @@ async function run() {
       core.setFailed(
         'Pull request number was neither set by user nor obtainable by context',
       )
+    } else {
+      console.log('PR Number: %s', prNumber)
     }
 
     if (!token) {
       console.warn('No token provided, might not work')
     }
     const octokit = new github.getOctokit(token)
+    console.log('octokit created')
+    console.log(octokit)
 
     const response = await octokit.pulls.get({
       owner: github.context.repo.owner,
